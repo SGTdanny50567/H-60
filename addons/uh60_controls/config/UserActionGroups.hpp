e009b9254593df38c8d10c734c5e2c2a4528ab0f
SGTdanny50567 <49874207+SGTdanny50567@users.noreply.github.com>
2022-03-11 02:40:19 +0000

    Add files via upload
    Added Modded Keybinding

---
+++ addons/uh60_controls/config/UserActionGroups.hpp
@@ -0,0 +1,30 @@
+class UserActionGroups 
+{
+    class uh60_controls //unique class name
+    {
+        name = "UH-60M Blackhawk Official"; // display name of your category
+        group[] = {
+            vxf_interaction
+            vtx_uh60m_c_help
+            vtx_uh60m_c_rmtsby
+            vtx_uh60_fd_raltToggle
+            vtx_uh60_flir_c_up
+            vtx_uh60_flir_c_down
+            vtx_uh60_flir_c_left
+            vtx_uh60_flir_c_right
+            vtx_uh60_flir_c_slew
+            vtx_uh60_flir_c_slewHMD
+            vtx_uh60_flir_c_zoom_inc
+            vtx_uh60_flir_c_zoom_desc
+            vtx_uh60m_hoistHookUp
+            vtx_uh60m_hoistHookDown
+            vtx_uh60m_hoistHookStabilize
+            vtx_uh60m_hoistHookGetIn
+            vtx_uh60m_c_slewup
+            vtx_uh60m_c_slewdown
+            vtx_uh60m_c_slewLeft
+            vtx_uh60m_c_slewright
+            vtx_uh60m_c_slewAction
+        }
+    }
+}
